c3507b369fad40a3159a40c433411fb568eff8b1
Cyril LM <clevymar@gmail.com>
2023-08-05 08:19:40 -0500

    ditto

--- common.py
+++ common.py
@@ -10,9 +10,9 @@ last_bd = end.strftime("%Y-%m-%d")
 EQUITY_UNDS = ['AAXJ', 'ACWI', 'AFK', 'ARGT', 'ARKK', 'BND', 'BNO', 'BWX', 'BWZ', 'CGGO', 'CGW', 'COPX', 'CORN', 'CYB', 'DBA', 'DBB', 'DBC', 'DEM', 'DFAX', 'DFEV', 'DFIV', 'DFLV', 'DFSV', 'DGS', 'DISV', 'DJP', 'ECOW', 'EEM', 'EEMV', 'EFA', 'EFG', 'EFV', 'EIDO', 'EIRL', 'EIS', 'EMLC', 'ENZL', 'EPHE', 'EPOL', 'EPP', 'EPU', 'ERUS', 'EUFN', 'EWA', 'EWC', 'EWD', 'EWG', 'EWH', 'EWI', 'EWJ', 'EWK', 'EWL', 'EWM', 'EWN', 'EWO', 'EWP', 'EWQ', 'EWS', 'EWT', 'EWU', 'EWW', 'EWY', 'EWZ', 'EYLD', 'EZA', 'EZU', 'FEZ', 'FM', 'FVAL', 'FXA', 'FXB', 'FXC', 'FXE', 'FXF', 'FXI', 'FXY', 'FYLD', 'GDX', 'GDXJ', 'GLD', 'GNR', 'GREK', 'GVAL', 'GXG', 'HAP', 'HYG', 'IDEV', 'IEF', 'IEMG', 'IEUR', 'IEV', 'IHY', 'ILF', 'INDY', 'IUSV', 'IVAL', 'IVE',
                'IWB', 'IWC', 'IWD', 'IWF', 'IWL', 'IWM', 'IWN', 'IWO', 'IWP', 'IWR', 'IWS', 'IYR', 'JKL', 'JNK', 'KBE', 'KCE', 'KIE', 'KRE', 'KWEB', 'LQD', 'MGK', 'MLXIX', 'NGE', 'NIB', 'NORW', 'OIH', 'PALL', 'PICB', 'PICK', 'PPLT', 'PRF', 'PXF', 'PXH', 'QQQ', 'QVAL', 'REM', 'RSX', 'SCJ', 'SHV', 'SHY', 'SIL', 'SLV', 'SOXX', 'SOYB', 'SPY', 'SYLD', 'THD', 'TIP', 'TLT', 'TUR', 'UDN', 'UGA', 'UNG', 'URA', 'USAI', 'USO', 'UUP', 'VBR', 'VCIT', 'VCLT', 'VCSH', 'VEA', 'VGK', 'VLUE', 'VNM', 'VNQ', 'VNQI', 'VONV', 'VOX', 'VPL', 'VTI', 'VTV', 'VWO', 'WEAT', 'WIP', 'XAR', 'XBI', 'XES', 'XHB', 'XHE', 'XHS', 'XLB', 'XLE', 'XLF', 'XLI', 'XLK', 'XLP', 'XLU', 'XLV', 'XLY', 'XME', 'XOP', 'XPH', 'XRT', 'XSD', 'XTN', '^HSCE', '^N225', '^STOXX50E']
 
-from random import choices
-EQUITY_UNDS = choices(EQUITY_UNDS,k=10)
-print('Selected: ',EQUITY_UNDS)
+# from random import choices
+# EQUITY_UNDS = choices(EQUITY_UNDS,k=10)
+# print('Selected: ',EQUITY_UNDS)
 
 # EQUITY_UNDS = ['AAXJ', 'ACWI', 'AFK', 'ARGT', 'ARKK', 'BND', 'BNO', 'IWB', 'IWC', 'IWD',
 #                'IEF', 'IEMG', 'IEUR', 'IEV', 'IHY', 'ILF', 'INDY', 'IUSV', 'IVAL', 'IVE',]
