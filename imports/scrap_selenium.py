7d63d33728283ec9cf2684ff4e691185d384445e
Cyril LM <clevymar@example.com>
2023-11-17 19:55:53 +0100

    ditto

--- imports/scrap_selenium.py
+++ imports/scrap_selenium.py
@@ -47,18 +47,19 @@ def start_driver(headless=True):
     """
     The function `start_driver` creates a headless Chrome driver with specific options     """
     try:
+        driver=None
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--log-level=3")
-        driver = webdriver.Chrome(options=chrome_options)
         if headless: 
             # chrome_options.add_argument('--window-size=1920,1080')
             chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--log-level=3")
+        driver = webdriver.Chrome(options=chrome_options)
 
         return driver
     except Exception as e:
-        driver.quit()       
+        if driver: driver.quit()       
         raise Exception("Could not create the driver") from e
          
 @timer
