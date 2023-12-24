1a62fd17fe9ce95b4f7644a0dbb1d1c97e83e314
Cyril LM <clevymar@example.com>
2023-12-24 10:02:56 +0100

    swaps - before rewrite

--- imports/import_swaps.py
+++ imports/import_swaps.py
@@ -1,85 +1,90 @@
-import pandas as pd
-import requests
-
 import os
 import sys
+from io import StringIO
+
+import pandas as pd
+import requests
+from icecream import ic
+
 currentdir = os.path.dirname(os.path.abspath(__file__))
 parentdir = os.path.dirname(currentdir)
 if parentdir not in sys.path:
     sys.path.insert(0, parentdir)
 
-
 from common import last_bd
 from utils.utils import timer
-from databases.database_mysql import SQLA_last_date, databases_update 
+from databases.database_mysql import SQLA_last_date, databases_update
 from databases.classes import Scrap
 
-IRS_ccies={ "EUR":"Europe_Europe_EUR",
-            "USD":"World_US_USD",
-            "JPY":"World_JP_JPY",
-            "CHF":"Europe_CH_CHF",
-            "GBP":"Europe_GB_GBP"
-            }
+IRS_ccies = {"EUR": "Europe_Europe_EUR", "USD": "World_US_USD", "JPY": "World_JP_JPY", "CHF": "Europe_CH_CHF", "GBP": "Europe_GB_GBP"}
 
 
-def get_oneIRS(ccy,verbose=True):
-    urlroot="https://produkte.erstegroup.com/CorporateClients/en/MarketsAndTrends/Fixed_Income/Capital_markets_derivatives/index.phtml?elem999058_index=Table_SwapRates_"
-    urlend="&elem999058_durationTimes=0"
-    urlpart=IRS_ccies[ccy]
-    url=urlroot+urlpart+urlend
+def get_oneIRS(ccy, verbose=True):
+    urlroot = "https://produkte.erstegroup.com/CorporateClients/en/MarketsAndTrends/Fixed_Income/Capital_markets_derivatives/index.phtml?elem999058_index=Table_SwapRates_"
+    # https://www.sparkasse.at/investments-en/markets/market-overview/money-market-fixed-income
+    
+    urlend = "&elem999058_durationTimes=0"
+    urlpart = IRS_ccies[ccy]
+    url = urlroot + urlpart + urlend
     r = requests.get(url)
     r_html = r.text
-    lst_df=pd.read_html(r_html)
-    df=lst_df[1]
-    df.columns=df.iloc[0]
-    df.drop(df.index[0],inplace=True)
-    tod=last_bd
-    df["CCY"]=ccy
-    df["Tenor"]=df["Name"].apply(lambda s: s[4:7].strip())
-    df["Date"]=tod
-    df["Rate"]=df["Current"]
-    df=df[["Date","CCY","Tenor","Rate"]]
-    if verbose:print(f"{ccy} IRS scrapped, {len(df)} tenors captured ")
+    ic(ccy + ': ' + url)
+    lst_df = pd.read_html(StringIO(r_html))
+    ic(lst_df)
+    df = lst_df[1]
+    df.columns = df.iloc[0]
+    df.drop(df.index[0], inplace=True)
+    tod = last_bd
+    df["CCY"] = ccy
+    df["Tenor"] = df["Name"].apply(lambda s: s[4:7].strip())
+    df["Date"] = tod
+    df["Rate"] = df["Current"]
+    df = df[["Date", "CCY", "Tenor", "Rate"]]
+    if verbose:
+        print(f"{ccy} IRS scrapped, {len(df)} tenors captured ")
     return df
-        
+
 
 def scrap_allIRS(verbose=True):
-    res=[]
+    res = []
     for ccy in IRS_ccies:
-        df=get_oneIRS(ccy)
+        df = get_oneIRS(ccy)
         res.append(df)
-    IRStable=pd.concat(res)
-    IRStable['Rate']=IRStable['Rate'].astype(float)
-    res= IRStable.copy()
+    IRStable = pd.concat(res)
+    IRStable["Rate"] = IRStable["Rate"].astype(float)
+    res = IRStable.copy()
     if verbose:
-        IRStable['Tenor']=IRStable['Tenor'].apply(lambda s:'0'+s if len(s)==2 else s)
-        pivot=pd.pivot_table(data=IRStable,values='Rate',index='Tenor',columns='CCY')
+        IRStable["Tenor"] = IRStable["Tenor"].apply(lambda s: "0" + s if len(s) == 2 else s)
+        pivot = pd.pivot_table(data=IRStable, values="Rate", index="Tenor", columns="CCY")
         print(pivot)
     return res
 
+
 @timer
 def IRS_toDB(verbose=False):
-    res=scrap_allIRS(verbose)
-    databases_update(res,"IRS_TS",idx=False,mode='append',verbose=verbose,save_insqlite=True)
+    res = scrap_allIRS(verbose)
+    databases_update(res, "IRS_TS", idx=False, mode="append", verbose=verbose, save_insqlite=True)
     return res
+
 
 @timer
 def import_swaps(verbose=True):
     try:
         res = IRS_toDB()
-        msg = f'Well downloaded !!! \n{len(res)} rows, {len(res.columns)} cols'
+        msg = f"Well downloaded !!! \n{len(res)} rows, {len(res.columns)} cols"
         return msg
     except Exception as e:
-        print('Error while downloading')
+        print("Error while downloading")
         print(e)
-        return 'Error while downloading'
+        return "Error while downloading"
 
 
 def swaps_last_date():
     return SQLA_last_date("IRS_TS")
 
+
 ScrapIRS = Scrap("IRS", IRS_toDB, swaps_last_date)
 
- 
-if __name__ == '__main__' :
+
+if __name__ == "__main__":
     print(import_swaps())
--- imports/scrap_selenium.py
+++ imports/scrap_selenium.py
@@ -9,10 +9,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.remote.webelement import WebElement
 from tqdm import tqdm
 
 import os
 import sys
+
 currentdir = os.path.dirname(os.path.abspath(__file__))
 parentdir = os.path.dirname(currentdir)
 if parentdir not in sys.path:
@@ -23,24 +25,51 @@ from common import last_bd, fichierTSUnderlyings
 from utils.utils import timer, print_color
 from databases.database_mysql import SQLA_read_table
 
-COLS_MORNINGSTAR=["ETF","P/E1","P/B","P/S","P/CF","DY","EG","HG","SG","CFG","BG","Composite","Last_updated","UpdateMode","URL","Name"]
-COLS_RATIOS=["EY","B/P","S/P","DY","Compo_Zscore"]
-SECTORS =["Basic Materials","Consumer Cyclical","Financial Services", "Real Estate","Communication Services",
-              "Energy","Industrials","Technology", "Consumer Defensive", "Healthcare", "Utilities"  ]
-
+COLS_MORNINGSTAR = [
+    "ETF",
+    "P/E1",
+    "P/B",
+    "P/S",
+    "P/CF",
+    "DY",
+    "EG",
+    "HG",
+    "SG",
+    "CFG",
+    "BG",
+    "Composite",
+    "Last_updated",
+    "UpdateMode",
+    "URL",
+    "Name",
+]
+COLS_RATIOS = ["EY", "B/P", "S/P", "DY", "Compo_Zscore"]
+SECTORS = [
+    "Basic Materials",
+    "Consumer Cyclical",
+    "Financial Services",
+    "Real Estate",
+    "Communication Services",
+    "Energy",
+    "Industrials",
+    "Technology",
+    "Consumer Defensive",
+    "Healthcare",
+    "Utilities",
+]
 
 
 class SeleniumError(Exception):
     pass
 
 
-benchmark='ACWI'
+benchmark = "ACWI"
 dictInput = json.load(open(fichierTSUnderlyings, "r"))
 fullList = dictInput["EQTY_SPOTS"]
-    
-ratios=[]
-initDict={x:"" for x in COLS_MORNINGSTAR}
-errs=[]
+
+ratios = []
+initDict = {x: "" for x in COLS_MORNINGSTAR}
+errs = []
 
 
 def _clean_price(s):
@@ -52,25 +81,48 @@ def _clean_price(s):
     except Exception as e:
         print(e)
         return None
-    
 
-def start_driver(headless=True,forCME=False):
+
+def details_element(element: WebElement):
+    id = element.get_attribute('id')
+    print(f'ID: {id}')
+
+    # Get its name
+    name = element.get_attribute('name')
+    print(f'Name: {name}')
+
+    # Get its text
+    text = element.text
+    print(f'Text: {text}')
+
+    # Get its class
+    class_name = element.get_attribute('class')
+    print(f'Class: {class_name}')
+
+    # Get its tag name
+    tag_name = element.tag_name
+    print(f'Tag Name: {tag_name}')
+
+def start_driver(headless=True, forCME=False):
     """
-    The function `start_driver` creates a headless Chrome driver with specific options     """
+    The function `start_driver` creates a headless Chrome driver with specific options"""
     from selenium import webdriver
+
     try:
-        driver=None
+        driver = None
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--no-sandbox")
         if forCME:
-            chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36")
+            chrome_options.add_argument(
+                "user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
+            )
             chrome_options.add_argument("--window-size=1920,1080")
             # chrome_options.add_argument("--start-maximized")
-            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--headless=new")
-            
+
         else:
-            if headless: 
+            if headless:
                 chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--log-level=3")
@@ -78,18 +130,19 @@ def start_driver(headless=True,forCME=False):
 
         return driver
     except Exception as e:
-        if driver: driver.quit()       
+        if driver:
+            driver.quit()
         raise Exception("Could not create the driver") from e
 
-         
+
 @timer
-def selenium_scrap_simple(link:str):
+def selenium_scrap_simple(link: str):
     """
     The function `selenium_scrap_simple` uses Selenium to scrape the HTML source code of a given link.
     :param link: The `link` parameter is a string that represents the URL of the webpage you want to
     scrape using Selenium
     """
-    html_source=None
+    html_source = None
     driver = start_driver()
     try:
         driver.get(link)
@@ -97,93 +150,92 @@ def selenium_scrap_simple(link:str):
     except Exception as e:
         raise Exception("Could not scrap the link at {link}") from e
     finally:
-        print_color('Quitting Selenium driver','COMMENT')
+        print_color("Quitting Selenium driver", "COMMENT")
         driver.quit()
     return html_source
 
 
-
-
-def _sub_getETF_Selenium(driver,ETF_name,exchange='arcx',verbose=True):
-    if exchange[:5]=='funds':
-        url=f"https://www.morningstar.com/{exchange}/{ETF_name}/portfolio"
-    else:  
-        foundURL=False
-        EXCHANGE_LIST=[exchange]+[c for c in ['arcx','xnys','xnas','bats'] if c!=exchange]
+def _sub_getETF_Selenium(driver, ETF_name, exchange="arcx", verbose=True):
+    if exchange[:5] == "funds":
+        url = f"https://www.morningstar.com/{exchange}/{ETF_name}/portfolio"
+    else:
+        foundURL = False
+        EXCHANGE_LIST = [exchange] + [c for c in ["arcx", "xnys", "xnas", "bats"] if c != exchange]
         for exc in EXCHANGE_LIST:
-            url=f"https://www.morningstar.com/etfs/{exc}/{ETF_name}/portfolio"
+            url = f"https://www.morningstar.com/etfs/{exc}/{ETF_name}/portfolio"
             r = requests.get(url)
-            if r.status_code==200:
-                foundURL=True
+            if r.status_code == 200:
+                foundURL = True
                 break
         if not foundURL:
-            raise SeleniumError('Correct Url could not be found for: '+ETF_name)
-    if verbose: print_color(f'\n[+]Scrapping for {ETF_name} on {exchange=} at {url=}','COMMENT')
+            raise SeleniumError("Correct Url could not be found for: " + ETF_name)
+    if verbose:
+        print_color(f"\n[+]Scrapping for {ETF_name} on {exchange=} at {url=}", "COMMENT")
     driver.get(url)
     wait = WebDriverWait(driver, 15)
-    element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'sal-sector-exposure__sector-table')))
-    element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'sal-measures__value-table')))
-    
+    element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "sal-sector-exposure__sector-table")))
+    element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "sal-measures__value-table")))
+
     html_source = driver.page_source
-    source_data = html_source.encode('utf-8')
+    source_data = html_source.encode("utf-8")
     soup = BeautifulSoup(source_data, "lxml")
-    
+
     compteur = 0
-    row_dict=initDict.copy()
-    row_dict["ETF"]=ETF_name
-    row_dict["Last_updated"]=last_bd
-    res=soup.find(class_="mdc-security-header__name")
+    row_dict = initDict.copy()
+    row_dict["ETF"] = ETF_name
+    row_dict["Last_updated"] = last_bd
+    res = soup.find(class_="mdc-security-header__name")
     # res=res.find('span')
-    nom=res.text
-    tmp=nom.splitlines()
-    tmp=[el for el in tmp if len(el)>=3]
-    row_dict["Name"]=tmp[0].strip()
+    nom = res.text
+    tmp = nom.splitlines()
+    tmp = [el for el in tmp if len(el) >= 3]
+    row_dict["Name"] = tmp[0].strip()
     """ find and grab financial ratios """
-    res=soup.find(class_='sal-measures__value-table')
-    res=res.find("tbody")    
+    res = soup.find(class_="sal-measures__value-table")
+    res = res.find("tbody")
     for row in res.find_all("tr"):
-        sratio=row.find_all("td")[1].text
-        if sratio!="":
+        sratio = row.find_all("td")[1].text
+        if sratio != "":
             try:
-                ratio=float(sratio) 
+                ratio = float(sratio)
             except:
-                """exception for P/CF not as important as others, replace by cat """
-                if row.find_all("td")[0].text.strip()=="Price/Cash Flow":
+                """exception for P/CF not as important as others, replace by cat"""
+                if row.find_all("td")[0].text.strip() == "Price/Cash Flow":
                     try:
-                        ratio=float(row.find_all("td")[2].text)
+                        ratio = float(row.find_all("td")[2].text)
                     except:
-                        ratio=float('nan')
-                else: 
-                    ratio=float('nan')
-            row_dict[COLS_MORNINGSTAR[compteur+1]]=ratio
-            #print "%s \t % 6.2f" % (cols[compteur+1],ratio)           
-            compteur+=1
-    #tbs=soup.findAll("tr", {"class": "ng-scope"})
+                        ratio = float("nan")
+                else:
+                    ratio = float("nan")
+            row_dict[COLS_MORNINGSTAR[compteur + 1]] = ratio
+            # print "%s \t % 6.2f" % (cols[compteur+1],ratio)
+            compteur += 1
+    # tbs=soup.findAll("tr", {"class": "ng-scope"})
     """ find and grab sector composition """
-    res=soup.find(class_='sal-sector-exposure__sector-table')
-    res=res.find("tbody")   
+    res = soup.find(class_="sal-sector-exposure__sector-table")
+    res = res.find("tbody")
     for row in res.find_all("tr"):
-        cells=row.find_all("td")
-        lbl=cells[0].text.strip()
+        cells = row.find_all("td")
+        lbl = cells[0].text.strip()
         if lbl in SECTORS:
             try:
-                weight=float(cells[1].text)
+                weight = float(cells[1].text)
             except:
-                weight=float('nan')
-            row_dict[lbl]=weight
-            compteur+=1
-    row_dict["Last_updated"]=last_bd
-    row_dict["UpdateMode"]='Selenium'
-    row_dict["URL"]=url
+                weight = float("nan")
+            row_dict[lbl] = weight
+            compteur += 1
+    row_dict["Last_updated"] = last_bd
+    row_dict["UpdateMode"] = "Selenium"
+    row_dict["URL"] = url
     return row_dict
 
 
 @timer
-def selenium_scrap_ratios(secList:list,verbose=True):
+def selenium_scrap_ratios(secList: list, verbose=True):
     """
     The function `selenium_scrap_ratios` uses Selenium to scrape ratios for a list of securities, and
     returns a DataFrame of the scraped data along with any errors encountered during scraping.
-    
+
     :param secList: The `secList` parameter is a list of securities for which you want to scrape ratios
     using Selenium. Each security in the list should be a string representing the security symbol or
     identifier
@@ -192,41 +244,40 @@ def selenium_scrap_ratios(secList:list,verbose=True):
     for which there were errors in scrapping with Selenium.
     """
     # using info from https://help.pythonanywhere.com/pages/selenium
-    res=[]
-    errs=[]
-    df=pd.DataFrame()
-    dfETFRef = SQLA_read_table('ETF_REF')
+    res = []
+    errs = []
+    df = pd.DataFrame()
+    dfETFRef = SQLA_read_table("ETF_REF")
 
     driver = start_driver()
     try:
         for sec in tqdm(secList):
-            exc='arcx'
-            if sec in dfETFRef['ETF'].tolist():
-                if dfETFRef.loc[dfETFRef['ETF']==sec,'DoNotRatio'].iloc[0]:
+            exc = "arcx"
+            if sec in dfETFRef["ETF"].tolist():
+                if dfETFRef.loc[dfETFRef["ETF"] == sec, "DoNotRatio"].iloc[0]:
                     continue
-                elif (special:=dfETFRef.loc[dfETFRef['ETF']==sec,'specialRatio'].iloc[0]) != "0":
-                    exc=special.split('_')[0]
+                elif (special := dfETFRef.loc[dfETFRef["ETF"] == sec, "specialRatio"].iloc[0]) != "0":
+                    exc = special.split("_")[0]
             try:
-                tmp = _sub_getETF_Selenium(driver,sec, exchange=exc,verbose=verbose)
+                tmp = _sub_getETF_Selenium(driver, sec, exchange=exc, verbose=verbose)
                 res.append(tmp.copy())
             except Exception as e:
-                print_color(f'[-]Error in scrapping with selenium for {sec}: \n',"FAIL")
+                print_color(f"[-]Error in scrapping with selenium for {sec}: \n", "FAIL")
                 print(e)
                 errs.append(sec)
-                
-        if len(res)>0:
-            df=pd.DataFrame(res)
-            print_color(f'[+] {len(df)} underlyings ratios scrapped',"RESULT")
-            if verbose: print(df)
+
+        if len(res) > 0:
+            df = pd.DataFrame(res)
+            print_color(f"[+] {len(df)} underlyings ratios scrapped", "RESULT")
+            if verbose:
+                print(df)
         else:
-            print_color(f'[-]No data was scrapped with selenium',"RESULT")
-            
-        if len(errs)>0:
-            print_color(f'[-]Errors in scrapping with selenium for {len(errs)} underlyings',"FAIL")
+            print_color(f"[-]No data was scrapped with selenium", "RESULT")
+
+        if len(errs) > 0:
+            print_color(f"[-]Errors in scrapping with selenium for {len(errs)} underlyings", "FAIL")
             print(errs)
     finally:
-        print_color('Quitting Selenium driver','COMMENT')
+        print_color("Quitting Selenium driver", "COMMENT")
         driver.quit()
-    return df,errs
-
-
+    return df, errs
