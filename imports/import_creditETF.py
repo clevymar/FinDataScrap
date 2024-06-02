0ceb3f8f8ceeeb542d544a8ece83531ef0ddab7c
Cyril LM <clevymar@example.com>
2024-06-02 16:01:58 +0200

    fixing creditETF

--- imports/import_creditETF.py
+++ imports/import_creditETF.py
@@ -1,8 +1,10 @@
-#%%
+# %%
 from datetime import date
 from pathlib import Path
-import os,sys
+import os
+import sys
 from dataclasses import dataclass
+from typing import Optional
 
 currentdir = os.path.dirname(os.path.abspath(__file__))
 parentdir = os.path.dirname(currentdir)
@@ -10,115 +12,121 @@ if parentdir not in sys.path:
     sys.path.insert(0, parentdir)
 
 
-import pandas as pd 
+import pandas as pd
 import requests
+from requests.exceptions import HTTPError
 from bs4 import BeautifulSoup
+from loguru import logger
 
 from common import last_bd
 from utils.utils import timer
-from databases.database_mysql import SQLA_last_date, databases_update 
+from databases.database_mysql import SQLA_last_date, databases_update
 from databases.classes import Scrap
 
 
+# %%
 
-#%%
 
 @dataclass
-class Asset():
-    name:str
-    url:str
-    
-@dataclass
-class webField():
-    name:str
-    class_:str
-    value:str=None
-    
-    
-defData=[
-    webField('Index','col-indexSeriesName'), #indexSeriesName 
-    webField('YtM','col-yieldToWorst'),
-    webField('OAS','col-optionAdjustedSpread'),
-    webField('modDuration','col-modelOad')
-    ]
+class Asset:
+    name: str
+    url: str
 
-listAssets=[
-    Asset('BRLN','https://www.ishares.com/us/products/329872/blackrock-floating-rate-loan-etf/'),
-    Asset('HYG', 'https://www.ishares.com/us/products/239565/ishares-iboxx-high-yield-corporate-bond-etf'),
-    Asset('LQD', 'https://www.ishares.com/us/products/239566/ishares-iboxx-investment-grade-corporate-bond-etf'),
+
+@dataclass
+class webField:
+    name: str
+    class_: str
+    value: Optional[str] = None
+
+
+defData = [
+    webField("Index", "col-indexSeriesName"),  # indexSeriesName
+    webField("YtM", "col-yieldToWorst"),
+    webField("OAS", "col-optionAdjustedSpread"),  #col-optionAdjustedSpread
+    webField("modDuration", "col-modelOad"),
+]
+
+listAssets = [
+    Asset("BRLN", "https://www.ishares.com/us/products/329872/blackrock-floating-rate-loan-etf/"),
+    Asset("HYG", "https://www.ishares.com/us/products/239565/ishares-iboxx-high-yield-corporate-bond-etf"),
+    Asset("LQD", "https://www.ishares.com/us/products/239566/ishares-iboxx-investment-grade-corporate-bond-etf"),
     # Asset('IEAC','https://www.ishares.com/uk/individual/en/products/251726/ishares-core-corp-bond-ucits-etf')
-    ]
+]
 
-#TODO add non US ETFs - probably need to use Selenium
+# TODO add non US ETFs - probably need to use Selenium
+
 
 def get_data(url):
-    r=requests.get(url)
+    r = requests.get(url)
+    r.raise_for_status()
     soup = BeautifulSoup(r.text, "lxml")
-    res=[]
+    res = []
     for field in defData:
-        cl=field.class_
-        value = soup.find('div',{'class':cl})
-        value = value.find('span',{'class':'data'}).text.strip()
-        field.value=value
-        res.append(field)
+        try:
+            cl = field.class_
+            value = soup.find("div", {"class": cl})
+            value = value.find("div", {"class": "data"}).text.strip()
+            field.value = value
+            res.append(field)
+        except:
+            logger.error(f"{cl} field missing for {url}")
     return res
 
 
 def retrieve_all_data():
-    listResults=[]
+    listResults = []
     for asset in listAssets:
         try:
-            print(f'Scrapping {asset.name}')
+            logger.info(f"Scrapping {asset.name} @ {asset.url}")
             name = asset.name
-            res =  get_data(asset.url)
-            tmp={'name':name}
+            res = get_data(asset.url)
+            tmp = {"name": name}
             for field in res:
-                tmp[field.name]=field.value
+                tmp[field.name] = field.value
             listResults.append(tmp)
+        except HTTPError as http_err:
+            logger.error(f"HTTP error occurred while processing {name}: {http_err}")
         except Exception as e:
-            print(f"Error processing {name}\n: {e}")
-            
-    return listResults
-        
+            logger.error(f"Error processing {name}\n: {e}")
 
-#%%
+    return listResults
+
+
+# %%
+
 
 @timer
 def creditETF_toDB(verbose=False):
     listResults = retrieve_all_data()
     dfRes = pd.DataFrame(listResults)
-    dfRes['YtM']=dfRes['YtM'].str.replace('%','').astype(float).round(2)
-    dfRes['OAS']=(dfRes['OAS'].str.replace('bps','').astype(float) / 100).round(2)
-    dfRes['modDuration']=dfRes['modDuration'].str.replace(' yrs','').astype(float).round(2)
-    dfRes['Date']=last_bd
+    dfRes["YtM"] = dfRes["YtM"].str.replace("%", "").astype(float).round(2)
+    dfRes["OAS"] = (dfRes["OAS"].str.replace("bps", "").astype(float) / 100).round(2)
+    dfRes["modDuration"] = dfRes["modDuration"].str.replace(" yrs", "").astype(float).round(2)
+    dfRes["Date"] = last_bd
     if verbose:
+        logger.info('Successful! Result:')
         print(dfRes)
-    databases_update(dfRes,"CREDIT_ETF_TS",idx=False,mode='append',verbose=verbose, save_insqlite=True)
+    databases_update(dfRes, "CREDIT_ETF_TS", idx=False, mode="append", verbose=verbose, save_insqlite=True)
     return dfRes
 
+
 def import_creditETF(verbose=False):
-    msg=None
+    msg = None
     try:
         res = creditETF_toDB(verbose=verbose)
-        msg = f'Well downloaded !!! \n{len(res)} rows, {len(res.columns)} cols'
+        msg = f"Well downloaded !!! \n{len(res)} rows, {len(res.columns)} cols"
     except Exception as e:
-        raise Exception('Error while downloading Credit ETF') from e
+        raise Exception("Error while downloading Credit ETF") from e
     return msg
+
 
 def creditETF_last_date():
     return SQLA_last_date("CREDIT_ETF_TS")
 
-# The line `ScrapGovies = Scrap("GOVIES", govies_toDB, govies_last_date)` is creating an instance of
-# the `Scrap` class and assigning it to the variable `ScrapGovies`. The `Scrap` class is defined
-# elsewhere in the code and takes three arguments: a name, a function to retrieve data, and a function
-# to get the last date of the data. In this case, the name is "GOVIES", the function to retrieve data
-# is `govies_toDB`, and the function to get the last date is `govies_last_date`.
+
 ScrapCreditETF = Scrap("CREDIT ETF", creditETF_toDB, creditETF_last_date)
 
- 
-if __name__ == '__main__' :
+
+if __name__ == "__main__":
     print(import_creditETF(True))
-    
-
-
-                 
