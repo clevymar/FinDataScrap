98756ed6f0413492efce4ef7e10310e56108aae3
Cyril LM <clevymar@example.com>
2025-03-11 20:16:48 +0100

    adding scrolling

--- imports/import_swaps.py
+++ imports/import_swaps.py
@@ -197,7 +197,12 @@ def scrap_allIRS(verbose=True):
         time.sleep(SLEEP_TIME)
         ic(section.text)
         if len(section.text) < 100:
-            console.log("Section not loading")
+            console.log("Section not loading - moving lower")
+            script = """
+                    arguments[0].scrollIntoView();
+                    window.scrollBy(0, -(window.innerHeight * arguments[1]));
+                    """
+            driver.execute_script(script, section,0.5)
             section = wait.until(EC.visibility_of_element_located((By.ID, "3")))
             ic(section.text)
             blocks = wait.until(EC.visibility_of_any_elements_located((By.CLASS_NAME, "gem-comp-compact-tab-navigation")))
